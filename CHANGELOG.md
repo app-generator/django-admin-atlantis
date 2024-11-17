42733cf0cb15179a682b77c9de0f85812d81d315
App Generator <51070104+app-generator@users.noreply.github.com>
2024-11-17 06:24:38 +0200

    v1.0.4 - Fix for Django5 & Links Update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # CHANGELOG
 
+## [1.0.4] 2024-11-17
+### Changes
+
+- Remove `length_is` filter
+  - Deprecated in Django 5.1
+- Update RM Links
+    - [Django Admin Atlantis](https://app-generator.dev/docs/products/django-libs/theme-atlantis.html) - **Documentation** & Support Links
+    - [Django Atlantis](https://app-generator.dev/product/atlantis-dark/django/) - The product that uses the library
+    - [Getting Started with Django](https://app-generator.dev/docs/technologies/django/index.html) - Learn how to code **Django** Projects
+
 ## [1.0.3] 2023-06-21
 ### Changes
 
--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
 name='django-admin-atlantis',
-version='1.0.3',
+version='1.0.4',
 zip_safe=False,
 packages=find_packages(),
 include_package_data=True,
 description='Modern template for Django admin interface (Atlantis BS4 Design)',
 long_description=README,
 long_description_content_type="text/markdown",
-url='https://appseed.us/product/atlantis-dark-pro/django/',
+url='https://app-generator.dev/docs/products/django-libs/theme-atlantis.html',
 author='AppSeed.us',
 author_email='support@appseed.us',
 license='EULA License',
